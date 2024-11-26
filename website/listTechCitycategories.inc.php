51f3023d8080c9e5be8ea2edff74af8f8181f707
Chris 💪🏽 <chris@christopher.resource.campus.njit.edu>
2024-11-26 12:46:23 -0500

    Phase 5 JavaScript

--- website/listTechCitycategories.inc.php
+++ website/listTechCitycategories.inc.php
@@ -1,13 +1,38 @@
 <h2>Tech City Categories</h2>
-<ul>
-<?php
-include_once("techcity.category.php");
-$categories = Category::getCategories();
-foreach ($categories as $category) {
-   $TechCategoryID = $category->TechCategoryID;
-   $name = $TechCategoryID . " - " . $category->TechCategoryCode . ", " . $category->TechCategoryName;
-   // Output category as a list item
-   echo "<li>$name</li>";
-}
-?>
-</ul>
+<script language="javascript">
+   function listbox_dblclick() {
+       document.categories.displaycategory.click()
+   }
+   function button_click(target) {
+       var userConfirmed = true;
+       if (target == 1) {
+           userConfirmed = confirm("Are you sure you want to remove this category?");
+       }
+       if (userConfirmed) {
+           if (target == 0) categories.action = "index.php?content=displaytechcitycategory";
+           if (target == 1) categories.action = "index.php?content=removeTechCitycategory";
+           if (target == 2) categories.action = "index.php?content=updatetechcategory";
+       } else {
+           alert("Action canceled.");
+       }
+   }
+</script>
+<h2>Select Category</h2>
+<form name="categories" method="post">
+   <select ondblclick="listbox_dblclick()" name="TechCategoryID" size="20">
+       <?php
+   //    include("category.php");
+       $categories = Category::getCategories();
+       foreach ($categories as $category) {
+           $TechCategoryID = $category->TechCategoryID;
+           $name = $TechCategoryID . " - " . $category->TechCategoryCode . ", " . $category->TechCategoryName;
+           echo "<option value=\"$TechCategoryID\">$name</option>\n";
+       }
+       ?>
+   </select>
+   <br>
+   <input type="submit" onClick="button_click(0)" name="displaycategory" value="View Category">
+   <input type="submit" onClick="button_click(1)" name="deletecategory" value="Delete Category">
+   <input type="submit" onClick="button_click(2)" name="updatecategory" value="Update Category">
+</form>
+
